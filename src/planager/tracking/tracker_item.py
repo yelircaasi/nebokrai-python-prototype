c46a0be78efd6414ba0bbc52ca7d21d2a84a034a
yelircaasi <isaac.r.riley@gmail.com>
2023-11-17 17:09:26 +0100

    feat: add additional prompt functions

--- src/planager/tracking/tracker_item.py
+++ src/planager/tracking/tracker_item.py
@@ -1,7 +1,15 @@
 from abc import ABCMeta, abstractmethod
-from typing import Any, Protocol, Union
+from typing import Any, Optional, Protocol, Union
 
 from ..configuration import path_manager
+from ..util import PDate, PTime
+from ..util.prompt import (
+    prompt_boolean,
+    prompt_integer,
+    prompt_natural,
+    prompt_natural_sequence,
+    prompt_time,
+)
 
 # class TrackerItem(Protocol):
 #     """
@@ -23,36 +31,14 @@ from ..configuration import path_manager
 #     def read_log_dict(self, log_dict: dict[str, Any]) -> None:
 #         ...
 
+
 class TrackerItem(Protocol):
     name: str
     prompt: str
     desirable: str
     item_type: str
     order: str
-
-    def prompt_interactively(self) -> None:
-        ...
-
-    def as_log_dict(self) -> dict[str, Any]:
-        ...
-
-    def read_log_dict(self, log_dict: dict[str, Any]) -> None:
-        ...
-
-
-class CompositeTrackerItem:
-    name: str
-    prompt: str
-    desirable: str
-    item_type: str
-    order: str
-
-    def __init__(self, item_dict: dict[str, Any]) -> None:
-        self.name: str = item_dict["name"]
-        self.prompt: str = item_dict["prompt"]
-        self.desirable: str = item_dict["desirable"]
-        self.item_type: str = item_dict["type"]
-        self.order: str = item_dict["order"]
+    response: Any
 
     def prompt_interactively(self) -> None:
         ...
@@ -67,9 +53,11 @@ class CompositeTrackerItem:
 class TimeTrackerItem:
     name: str
     prompt: str
+    error_prompt: str = ""
     desirable: str
     item_type: str
     order: str
+    response: Optional[PTime]
 
     def __init__(self, item_dict: dict[str, Any]) -> None:
         self.name: str = item_dict["name"]
@@ -77,9 +65,10 @@ class TimeTrackerItem:
         self.desirable: str = item_dict["desirable"]
         self.item_type: str = item_dict["type"]
         self.order: str = item_dict["order"]
+        self.response = None
 
     def prompt_interactively(self) -> None:
-        ...
+        self.response = prompt_time(self.prompt, self.error_prompt)
 
     def as_log_dict(self) -> dict[str, Any]:
         return {}
@@ -91,9 +80,11 @@ class TimeTrackerItem:
 class NaturalTrackerItem:
     name: str
     prompt: str
+    error_prompt: str
     desirable: str
     item_type: str
     order: str
+    response: Optional[int]
 
     def __init__(self, item_dict: dict[str, Any]) -> None:
         self.name: str = item_dict["name"]
@@ -103,7 +94,7 @@ class NaturalTrackerItem:
         self.order: str = item_dict["order"]
 
     def prompt_interactively(self) -> None:
-        ...
+        self.response = prompt_natural(self.prompt, self.error_prompt)
 
     def as_log_dict(self) -> dict[str, Any]:
         return {}
@@ -115,9 +106,11 @@ class NaturalTrackerItem:
 class BoolTrackerItem:
     name: str
     prompt: str
+    error_prompt: str = ""
     desirable: str
     item_type: str
     order: str
+    response: Optional[bool]
 
     def __init__(self, item_dict: dict[str, Any]) -> None:
         self.name: str = item_dict["name"]
@@ -125,9 +118,10 @@ class BoolTrackerItem:
         self.desirable: str = item_dict["desirable"]
         self.item_type: str = item_dict["type"]
         self.order: str = item_dict["order"]
-
+        self.response: Optional[bool]
+        
     def prompt_interactively(self) -> None:
-        ...
+        self.response = prompt_boolean(self.prompt)
 
     def as_log_dict(self) -> dict[str, Any]:
         return {}
@@ -136,12 +130,40 @@ class BoolTrackerItem:
         ...
 
 
-class SequenceTrackerItem:
+class NaturalSequenceTrackerItem:
     name: str
     prompt: str
+    error_prompt: str = ""
     desirable: str
     item_type: str
     order: str
+    response: Optional[list[Any]]
+
+    def __init__(self, item_dict: dict[str, Any]) -> None:
+        self.name: str = item_dict["name"]
+        self.prompt: str = item_dict["prompt"]
+        self.desirable: str = item_dict["desirable"]
+        self.item_type: str = item_dict["type"]
+        self.order: str = item_dict["order"]
+
+    def prompt_interactively(self) -> None:
+        self.response = prompt_natural_sequence(self.prompt, self.error_prompt)
+
+    def as_log_dict(self) -> dict[str, Any]:
+        return {}
+
+    def read_log_dict(self, log_dict: dict[str, Any]) -> None:
+        ...
+
+
+class CompositeTrackerItem: #TODO; recursive behavior makes this significantly trickier than others
+    name: str
+    prompt: str
+    error_prompt: str = ""
+    desirable: str
+    item_type: str
+    order: str
+    response: Any
 
     def __init__(self, item_dict: dict[str, Any]) -> None:
         self.name: str = item_dict["name"]
@@ -158,11 +180,6 @@ class SequenceTrackerItem:
 
     def read_log_dict(self, log_dict: dict[str, Any]) -> None:
         ...
-
-
-# TrackerItem = Union[
-#     CompositeTrackerItem, TimeTrackerItem, NaturalTrackerItem, BoolTrackerItem, SequenceTrackerItem
-# ]
 
 
 def get_tracker_item(item_dict: dict[str, Union[str, list, dict]]) -> TrackerItem:
@@ -176,6 +193,6 @@ def get_tracker_item(item_dict: dict[str, Union[str, list, dict]]) -> TrackerIte
         case "boolean":
             return BoolTrackerItem(item_dict)
         case "[]natural":
-            return SequenceTrackerItem(item_dict)
+            return NaturalSequenceTrackerItem(item_dict)
         case _:
             raise ValueError(f"{item_dict['type']} is not a valid tracker item type.")
--- src/planager/util/prompt.py
+++ src/planager/util/prompt.py
@@ -1,4 +1,10 @@
-def prompt_integer(prompt_message: str, invalid_input_message: str) -> int:
+import re
+from typing import Iterable
+
+from .pdatetime import PTime
+
+
+def prompt_integer(prompt_message: str, invalid_input_message: str = "") -> int:
     """
     Interactively prompts for an integer until an integer is given.
     """
@@ -6,6 +12,91 @@ def prompt_integer(prompt_message: str, invalid_input_message: str) -> int:
     while not isvalid:
         try:
             value = int(input(prompt_message))
+            isvalid = True
+        except ValueError:
+            print(invalid_input_message)
+            continue
+    return value
+
+
+def prompt_boolean(
+    prompt_message: str,
+    valid_true: Iterable[str] = {"y", "true", "done", "yes", "yep"},
+) -> bool:
+    """
+    Interactively prompts for an boolean. Inputs in 'valid_true' are treated as True; others are
+      interpreted as False.
+    """
+    value = input(prompt_message).strip().lower() in valid_true
+    return value
+
+
+def prompt_natural(prompt_message: str, invalid_input_message: str = "") -> int:
+    """
+    Interactively prompts for an integer until an integer is given.
+    """
+    isvalid = False
+    while not isvalid:
+        try:
+            value = int(input(prompt_message))
+            assert value >= 0
+            isvalid = True
+        except ValueError:
+            print(invalid_input_message)
+            continue
+    return value
+
+
+def prompt_integer_sequence(
+    prompt_message: str,
+    invalid_input_message: str = (
+        "Invalid input. Please enter a sequence of integers separated by spaces, commas, or both."
+    ),
+) -> int:
+    """
+    Interactively prompts for an integer until an integer is given.
+    """
+    isvalid = False
+    while not isvalid:
+        try:
+            value = int(input(prompt_message))
+            isvalid = True
+        except ValueError:
+            print(invalid_input_message)
+            continue
+    return value
+
+
+def prompt_natural_sequence(
+    prompt_message: str,
+    invalid_input_message: str = (
+        "Invalid input. Please enter a sequence of natural numbers separated by spaces, commas, or both."
+    ),
+) -> list[int]:
+    """
+    Interactively prompts for an integer until an integer is given.
+    """
+    isvalid = False
+    while not isvalid:
+        try:
+            input_str = input(prompt_message).strip()
+            value = list(map(int, re.split("[, ]+", input_str)))
+            assert all(map(lambda x: x >= 0, value))
+            isvalid = True
+        except ValueError:
+            print(invalid_input_message)
+            continue
+    return value
+
+
+def prompt_time(prompt_message: str, invalid_input_message: str = "") -> PTime:
+    """
+    Interactively prompts for an integer until an integer is given.
+    """
+    isvalid = False
+    while not isvalid:
+        try:
+            value = PTime.from_string(input(prompt_message))
             isvalid = True
         except ValueError:
             print(invalid_input_message)
