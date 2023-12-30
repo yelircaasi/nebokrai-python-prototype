959328809944d530f07867e443016327194b3eb2
yelircaasi <isaac.r.riley@gmail.com>
2023-12-30 17:49:47 +0100

    feat: add nix flake for development and ensure that poetry2nix is working

---
+++ flake-old.nix
@@ -0,0 +1,45 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    {
+      # Nixpkgs overlay providing the application
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: prev: {
+          # The application
+          nebokrai = prev.poetry2nix.mkPoetryEnv {
+            projectDir = ./.;
+            editablePackageSources = {
+              nebokrai = ./src/nebokrai;
+            };
+          };
+        })
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in
+      rec {
+        devShells = {
+          nebokrai = pkgs.mkreports;
+        };
+
+        packages = {
+          nebokrai = pkgs.nebokrai;
+        };
+        devShell = devShells.nebokrai;
+      }));
+}
+
